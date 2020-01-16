e7381b38e6cac32601fe16fac0924c7622f0d483
Kyrsten <kyrsten@diesdas.digital>
2020-01-16 14:04:10 +0100

    added information at the front

--- index.js
+++ index.js
@@ -1,121 +1,130 @@
 const express = require("express");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const hb = require("express-handlebars");
 const fs = require("fs");
 const list = fs.readdirSync(__dirname + "/projects");
 const finalYear = fs.readdirSync(__dirname + "/public/artsPortfolio/final");
-const firstHalfSecond = fs.readdirSync(__dirname + "/public/artsPortfolio/firstHalfSecond");
-const secondHalfSecond = fs.readdirSync(__dirname + "/public/artsPortfolio/secondHalfSecond");
+const firstHalfSecond = fs.readdirSync(
+  __dirname + "/public/artsPortfolio/firstHalfSecond"
+);
+const secondHalfSecond = fs.readdirSync(
+  __dirname + "/public/artsPortfolio/secondHalfSecond"
+);
 const firstYear = fs.readdirSync(__dirname + "/public/artsPortfolio/firstYear");
 
 let secretEmail;
 let secretPass;
 if (process.env.email) {
-    secretEmail = process.env.email;
-    secretPass = process.env.pass;
+  secretEmail = process.env.email;
+  secretPass = process.env.pass;
 } else {
-    const secret = require("./secrets");
-    secretEmail = `${secret.email}`;
-    secretPass = `${secret.pass}`;
+  const secret = require("./secrets");
+  secretEmail = `${secret.email}`;
+  secretPass = `${secret.pass}`;
 }
 
 const app = express();
 const projectList = list.map(i => {
-    const { name, tech, desc, web } = require(__dirname + "/projects/" + i + "/info");
-    return {
-        i,
-        name,
-        tech,
-        desc,
-        web
-    };
+  const { name, tech, desc, web } = require(__dirname +
+    "/projects/" +
+    i +
+    "/info");
+  return {
+    i,
+    name,
+    tech,
+    desc,
+    web
+  };
 });
 
-const bigProjectList = require(__dirname + "/public/bigProjectList/bigProjectList.json")
+const bigProjectList = require(__dirname +
+  "/public/bigProjectList/bigProjectList.json");
 
- // View engine setup
+// View engine setup
 app.engine("handlebars", hb());
 app.set("view engine", "handlebars");
 
-
 app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
+  bodyParser.urlencoded({
+    extended: false
+  })
 );
 app.use(bodyParser.json());
 
 app.use(express.static("public"));
 app.use(express.static("projects"));
 
-
 ///////////////////////////////Xpress handlebars///////////////////
 
 app.get("/", (req, res) => {
-    // res.redirect("/projects");
-    res.render("welcome", {
-        layout: "main",
-        projectList
-    });
+  // res.redirect("/projects");
+  res.render("welcome", {
+    layout: "main",
+    projectList
+  });
+});
+
+app.get("/oldWebsite", (req, res) => {
+  res.render("home", {
+    layout: "main",
+    projectList
+  });
 });
 
 app.get("/projects", (req, res) => {
-    res.render("projects", {
-        layout: "main",
-        projectList,
-        bigProjectList
-    });
+  res.render("projects", {
+    layout: "main",
+    projectList,
+    bigProjectList
+  });
 });
 
 app.get("/projects/:projName/description", (req, res) => {
-    let projName = req.params.projName;
-    let tempProjList;
-    let test = projectList.map(x => {
-        if (projName == x.i) {
-            tempProjList = x;
-            }
-        });
-        res.render("desc", {
-            layout: "main",
-            tempProjList
-        });
-
+  let projName = req.params.projName;
+  let tempProjList;
+  let test = projectList.map(x => {
+    if (projName == x.i) {
+      tempProjList = x;
+    }
+  });
+  res.render("desc", {
+    layout: "main",
+    tempProjList
+  });
 });
 app.get("/projects/:projId/bigProjectdescription", (req, res) => {
-    let projId = req.params.projId;
-    let tempBigProjList;
-    for (let key in bigProjectList) {
-        if (bigProjectList[key].id == projId) {
-            tempBigProjList = bigProjectList[key];
-        }
+  let projId = req.params.projId;
+  let tempBigProjList;
+  for (let key in bigProjectList) {
+    if (bigProjectList[key].id == projId) {
+      tempBigProjList = bigProjectList[key];
     }
-        res.render("bigProjDesc", {
-            layout: "main",
-            tempBigProjList
-        });
+  }
+  res.render("bigProjDesc", {
+    layout: "main",
+    tempBigProjList
+  });
 });
 
 app.get("/artsPortfolio", (req, res) => {
-        res.render("artsPortfolio", {
-            layout: "main",
-            finalYear,
-            firstHalfSecond,
-            secondHalfSecond,
-            firstYear
-        });
+  res.render("artsPortfolio", {
+    layout: "main",
+    finalYear,
+    firstHalfSecond,
+    secondHalfSecond,
+    firstYear
+  });
 });
 
-
 app.get("/aboutMe", (req, res) => {
-    res.render("aboutMe", {
-        layout: "main"
-    })
-})
-
-
+  res.render("aboutMe", {
+    layout: "main"
+  });
+});
 
 app.post("/send", (req, res) => {
-    const output = `
+  const output = `
         <p>You have a new contact request</p>
         <h3>Contact Details</h3>
         <ul>
@@ -126,31 +135,31 @@ app.post("/send", (req, res) => {
         </ul>
         <h3>Message:</h3>
         <p>${req.body.message}</p>
-    `
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: `${secretEmail}`,
-            pass: `${secretPass}`
-        }
-    });
-    const mailOptions = {
-        from: `${req.body.email}`,
-        to: `${secretEmail}`,
-        subject: 'Subject of your email',
-        html: output
-    };
-    transporter.sendMail(mailOptions, function (err, info) {
-        if(err)
-            console.log(err)
-        else
-            console.log(info);
-    });
-    res.render("aboutMe", {msg: "Thank you, the Email is on it's way to me!"})
-})
-
-app.get("*", function(req, res) {
-    res.redirect("/");
+    `;
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: `${secretEmail}`,
+      pass: `${secretPass}`
+    }
+  });
+  const mailOptions = {
+    from: `${req.body.email}`,
+    to: `${secretEmail}`,
+    subject: "Subject of your email",
+    html: output
+  };
+  transporter.sendMail(mailOptions, function(err, info) {
+    if (err) console.log(err);
+    else console.log(info);
+  });
+  res.render("aboutMe", { msg: "Thank you, the Email is on it's way to me!" });
 });
 
-app.listen(process.env.PORT || 8080, () => console.log("listening"));
+app.get("*", function(req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 8080, () =>
+  console.log(`listening on PORT: ${process.env.PORT || 8080}`)
+);
